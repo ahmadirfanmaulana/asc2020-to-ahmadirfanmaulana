00b1bd0bc23076567b327e9a6ab25a207bea4443
Ahmad Irfan Maulana <email.ahmadirfan@gmail.com>
2020-11-04 13:59:52 +0700

    update'

--- module_cms/wp-content/themes/Kazan_MuseumTour/functions.php
+++ module_cms/wp-content/themes/Kazan_MuseumTour/functions.php
@@ -1,1 +1,91 @@
 <?php
+
+// init
+add_action('init', function () {
+
+    // register social menu
+    register_post_type('social-media', [
+       'label' => 'Social Media Links',
+       'public' => true,
+       'menu_icon' => 'dashicons-share',
+    ]);
+
+});
+
+// login style
+add_action('login_enqueue_scripts', function () {
+    $museums = get_museums();
+    $rand = rand(0, count($museums)-1);
+    $item = $museums[$rand];
+   ?>
+    <style>
+        #login h1 a {
+            background: unset;
+        }
+        body {
+            background: url("<?= get_the_post_thumbnail_url($item->ID); ?>") !important;
+            background-size: cover;
+        }
+        body::before {
+            width: 100vw;
+            height: 100vh;
+            background: rgba(0,0,0,0.8);
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 0;
+        }
+        #login {
+            position: relative;
+            z-index: 99;
+        }
+        .login #backtoblog a, .login #nav a{
+            color: #fff !important;
+        }
+    </style>
+    <?php
+});
+
+// scripts
+add_action('wp_enqueue_scripts', function () {
+   wp_enqueue_script('kazan-museum-script', get_stylesheet_directory_uri() . '/js/main.js');
+   wp_enqueue_style('kazan-museum-style', get_stylesheet_directory_uri() . '/css/main.css');
+});
+
+// get museums
+function get_museums () {
+    $args = [
+        'post_type' => 'page',
+        'posts_per_page' => -1,
+    ];
+
+    $posts = get_posts( $args );
+
+    $museums = [];
+
+    foreach ($posts as $post) {
+        if ($post->post_title != 'News' && $post->post_title != 'Museums') {
+            $museums[] = $post;
+        }
+    }
+
+    return $museums;
+}
+
+// get news
+function get_news ($state) {
+    $args = [
+        'post_type' => 'post',
+    ];
+
+    if ($state == 'limit') {
+        $args['posts_per_page'] = 4;
+    } else {
+        $args['posts_per_page'] = -1;
+    }
+
+    $posts = get_posts( $args );
+
+    return $posts;
+}
+
--- module_cms/wp-content/themes/Kazan_MuseumTour/index.php
+++ module_cms/wp-content/themes/Kazan_MuseumTour/index.php
@@ -1,10 +1,110 @@
-<?php get_header(); ?>
-<main id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+if (!isset($_POST['from_ajax'])) {
+    get_header();
+}
+?>
+
+    <main id="content">
+
+        <!-- Hero Section -->
+        <section class="hero" id="hero">
+            <div class="container">
+                <div class="hero-caption">
+                    <h1 class="hero-title">Kazan Museum Tour</h1>
+                    <h3 class="hero-subtitle">
+                        The most amazing museum in the world.
+                    </h3>
+                    <a href="#list-museum" class="btn btn-cta">Discover Museum</a>
+                </div>
+            </div>
+        </section>
+        <!-- Hero Section -->
+
+        <!-- Museum Section -->
+        <section class="museum-list" id="museum-list">
+            <div class="section-header">
+                <div class="container">
+                    <h2 class="section-title">Hotest Museums</h2>
+                    <a href="" class="link">All Museums</a>
+                </div>
+            </div>
+
+            <div class="container">
+
+                <div class="museum-wrapper">
+
+                    <?php foreach (get_museums() as $item) : ?>
+                    <a href="<?= get_the_permalink($item->ID) ?>" class="museum-item">
+                        <div class="museum-image" style="background:url(<?= get_the_post_thumbnail_url($item->ID); ?>); background-size: cover; background-position:center;"></div>
+                        <div class="museum-caption">
+                            <h3 class="museum-title"><?= $item->post_title; ?></h3>
+                        </div>
+                    </a>
+                    <?php endforeach; ?>
+
+                </div>
+            </div>
+        </section>
+        <!-- Museum Section -->
+
+        <!-- Main Layout -->
+        <div class="main-layout">
+
+            <div class="container">
+                <!-- News Section -->
+                <section class="news-list" id="museum-list">
+                    <div class="section-header">
+                        <div class="container">
+                            <h2 class="section-title">News</h2>
+                            <a href="" class="link">All news</a>
+                        </div>
+                    </div>
+
+
+                    <div class="news-wrapper">
+
+                        <?php foreach (get_news('limit') as $item) : ?>
+                            <a href="<?= get_the_permalink($item->ID) ?>" class="news-item">
+                                <div class="news-image" style="background:url(<?= get_the_post_thumbnail_url($item->ID); ?>); background-size: cover; background-position:center;"></div>
+                                <div class="news-caption">
+                                    <h3 class="news-title"><?= $item->post_title; ?></h3>
+                                </div>
+                            </a>
+                        <?php endforeach; ?>
+
+                    </div>
+                </section>
+                <!-- News Section -->
+
+                <!-- Cover Section -->
+                <section class="cover-list" id="cover-list">
+                    <div class="section-header">
+                        <div class="container">
+                            <h2 class="section-title">Cover</h2>
+                        </div>
+                    </div>
+
+                    <div class="news-wrapper">
+
+                        <?php foreach (get_museums() as $item) : ?>
+                            <a href="<?= get_the_permalink($item->ID) ?>" class="cover-item">
+                                <div class="cover-image" style="background:url(<?= get_the_post_thumbnail_url($item->ID); ?>); background-size: cover; background-position:center;"></div>
+                            </a>
+                        <?php endforeach; ?>
+
+                    </div>
+                </section>
+                <!-- Cover Section -->
+            </div>
+
+        </div>
+        <!-- Main Layout -->
+
+    </main>
+
+<?php
+if (!isset($_POST['from_ajax'])) {
+    get_footer();
+}
+?>
+
